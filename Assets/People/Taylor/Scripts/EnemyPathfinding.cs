006ac4c13210b1d8a5c90ccdf6f4ed15364913be
MUKILTEO\1726619 <1726619@mukilteo.wednet.edu>
2025-11-19 11:14:05 -0800

    enemy pathfinding first stage

--- Assets/People/Taylor/Scripts/EnemyPathfinding.cs
+++ Assets/People/Taylor/Scripts/EnemyPathfinding.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.Experimental.GraphView;
 
 public class EnemyPathfinding : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float moveSpeed = 2f;
+    public Rigidbody2D rb;
+
+    private Transform target;
+    private int pathIndex = 0;
+
+    private void Start()
     {
-        
+        target = LevelManager.main.path[pathIndex];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Vector2.Distance(target.position, transform.position) <= 0.1f)
+        {
+            pathIndex++;
+            if (pathIndex >= LevelManager.main.path.Length)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            else
+            {
+                target = LevelManager.main.path[pathIndex];
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        Vector2 direction = (target.position - transform.position).normalized;
+
+        rb.linearVelocity = direction * moveSpeed;
     }
 }
---
+++ Assets/People/Taylor/Scripts/LevelManager.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager main;
+
+    public Transform startPoint;
+    public Transform[] path;
+    private void Awake()
+    {
+        main = this;
+    }
+}
