ea44844a7b397f452ddd62206213985c22e8a133
MUKILTEO\1726619 <1726619@mukilteo.wednet.edu>
2025-11-21 12:21:14 -0800

    Turret is supposed to work. Someone take this dude to the land of misfit toys cause he don't work xD

--- Assets/People/Taylor/Scripts/Turret.cs
+++ Assets/People/Taylor/Scripts/Turret.cs
@@ -7,11 +7,16 @@ public class Turret : MonoBehaviour
     [Header("References")]
     [SerializeField] private Transform turretRotationPoint;
     [SerializeField] private LayerMask enemyMask;
-    [SerializeField] private float targetingRange = 5f;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firingPoint;
 
     [Header("Attribute")]
     [SerializeField] private float range = 5f;
+    [SerializeField] private float bps = 1f;
+    [SerializeField] private float targetingRange = 5f;
+
     private Transform target;
+    private float timeUntilFire;
 
     private void Update()
     {
@@ -22,6 +27,26 @@ public class Turret : MonoBehaviour
         }
 
         RotateTowardsTarget();
+
+        if (!CheckTargetIsInRange())
+        {
+            target = null;
+        }
+        else
+        {
+            timeUntilFire -= Time.deltaTime;
+
+            if (timeUntilFire >= 1f / bps)
+            {
+                Shoot();
+                timeUntilFire = 0f;
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        Debug.Log("Shooted");
     }
     private void FindTarget()
     {
@@ -33,6 +58,10 @@ public class Turret : MonoBehaviour
         }
     }
 
+    private bool CheckTargetIsInRange()
+    {
+        return Vector2.Distance(transform.position, target.position) <= range;
+    }
     private void RotateTowardsTarget()
     { 
         float angle  = Mathf.Atan2(target.position.y - transform.position.y, target.position.x - transform.position.x) * Mathf.Rad2Deg;
