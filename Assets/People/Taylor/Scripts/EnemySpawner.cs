e8f7f72d7d0dbe8d19a8e9cc6bc196cffaa648af
MUKILTEO\1726619 <1726619@mukilteo.wednet.edu>
2025-11-25 11:50:01 -0800

    Started on the Enemy Spawner script

---
+++ Assets/People/Taylor/Scripts/EnemySpawner.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject[] enemyPrefabs;
+
+    [Header("Attributes")]
+    [SerializeField] private int baseEnemies = 8;
+    [SerializeField] private float enemiesPerSecond = 0.5f;
+    [SerializeField] private float timeBetweenWaves = 5f;
+    [SerializeField] private float difficultyScalingFactor = 0.75f;
+
+    private int currentWave = 1;
+    private float timeSinceLastSpawn;
+    private int enemiesAlives;
+    private int enemiesLeftToSpawn;
+    private bool isSpawning = false;
+
+    private void Start()
+    {
+        StartWave();
+    }
+
+    private void Update()
+    {
+        if (!isSpawning) return;
+
+        timeSinceLastSpawn += Time.deltaTime; 
+
+        if(timeSinceLastSpawn >= (1f / enemiesPerSecond))
+        {
+            Debug.Log("Er hat gespawndet");
+        }
+    }
+
+    private void StartWave()
+    {
+        isSpawning = true;
+        enemiesLeftToSpawn = EnemiesPerWave();
+    }
+
+    private int EnemiesPerWave()
+    {
+        return Mathf.RoundToInt(baseEnemies * Mathf.Pow(currentWave, difficultyScalingFactor));
+    }
+}
