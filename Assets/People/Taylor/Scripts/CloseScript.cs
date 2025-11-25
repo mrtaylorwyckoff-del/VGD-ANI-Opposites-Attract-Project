a7f4ed71d5dfbdc24ad0658ebae59dec1bf33114
MUKILTEO\1725364 <1725364@mukilteo.wednet.edu>
2025-11-25 13:10:15 -0800

    Forgot what I did, just know it was Gigma.

--- Assets/People/Taylor/Scripts/CloseScript.cs
+++ Assets/People/Taylor/Scripts/CloseScript.cs
@@ -1,6 +1,46 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class CloseScript
+public class CloseScript : MonoBehaviour
 {
-    
+    public string sceneToQuitOn = "Quit";
+    public float quitDelay = 0.5f;
+
+    public Vector3 startSize;
+    public Vector3 endSize;
+
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name == sceneToQuitOn)
+        {
+            StartCoroutine(QuitGameAfterDelay(quitDelay));
+        }
+    }
+
+    IEnumerator QuitGameAfterDelay(float delay)
+    {
+        for (float t = 0; t < 1; t += Time.deltaTime)
+        {
+            transform.localScale = Vector3.Lerp(startSize, endSize, t);
+            yield return new WaitForEndOfFrame();
+        }
+
+        yield return new WaitForSeconds(delay);
+
+        QuitGame();
+    }
+
+    void QuitGame()
+    {
+        Debug.Log("Quitting application...");
+
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
 }
+
