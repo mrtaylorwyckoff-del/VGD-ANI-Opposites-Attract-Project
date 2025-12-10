5ed3921ebbb1c881191da9c3922b7e75cfeedb03
MUKILTEO\1725646 <1725646@mukilteo.wednet.edu>
2025-12-10 12:08:33 -0800

    enemy anijmations!!!

--- Assets/People/Taylor/Scripts/EnemyPathfinding.cs
+++ Assets/People/Taylor/Scripts/EnemyPathfinding.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 
 public class EnemyPathfinding : MonoBehaviour
 {
+    private Animator animator;
+
     [Header("References")]
     [SerializeField] private Rigidbody2D rb; 
     
@@ -16,6 +18,8 @@ public class EnemyPathfinding : MonoBehaviour
     private void Start()
     {
         target = LevelManager.main.path[pathIndex];
+
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -34,6 +38,25 @@ public class EnemyPathfinding : MonoBehaviour
                 target = LevelManager.main.path[pathIndex];
             }
         }
+
+        //Set the animator to play the correct animation based on movement direction
+        float angle = Mathf.Atan2(rb.linearVelocity.y, rb.linearVelocity.x) * Mathf.Rad2Deg;
+        animator.SetFloat("Angle", angle);
+
+        if (rb.linearVelocity.magnitude > 0.1f)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+
     }
     private void FixedUpdate()
     {
